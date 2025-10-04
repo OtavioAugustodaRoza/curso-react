2ddccb7ee58a4c485aaee69d8035a31e4fcdf0a7
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2025-10-03 21:58:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Titulo from "./titulo"
+function App(){
+   return( 
+    <div>
+      <Titulo nome ="otávio" cor = "red" /> 
+      <Titulo />
+    </div>
+
+   )
+}
+export default App
---
+++ src/Titulo.jsx
@@ -0,0 +1,9 @@
+function Titulo({nome}, {cor}){
+    return (
+        <div>
+             <h1 style={{color:cor}}>oi eu sou o {nome}</h1>
+             <h1>oi eu sou um titulo</h1>
+        </div>
+    )
+}
+export default Titulo
