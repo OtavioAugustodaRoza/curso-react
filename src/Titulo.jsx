ac7e6dac349a8ffb6e964e4e88bdcdf996d59031
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2025-10-03 21:59:44 -0300

    fix: bug

--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -1,7 +1,7 @@
-function Titulo({nome}, {cor}){
+function Titulo({nome}){
     return (
         <div>
-             <h1 style={{color:cor}}>oi eu sou o {nome}</h1>
+             <h1>oi eu sou o {nome}</h1>
              <h1>oi eu sou um titulo</h1>
         </div>
     )
